c81f4684f50b3a10d33100456765514001c2634d
iheteroclite <118612115+iheteroclite@users.noreply.github.com>
2022-11-25 16:04:12 +0000

    Fixed issue #1
    lines variable replaces line1 etc, and join method used on the lines list to create return output (in arithmetic_formatter.py)

--- boilerplate-arithmetic-formatter/arithmetic_arranger.py
+++ boilerplate-arithmetic-formatter/arithmetic_arranger.py
@@ -2,18 +2,17 @@ import sys
 import re
 
 def arithmetic_arranger(problems, calculate=False):
-    # Check input params (problems), more than 5 items should error
-    if len(problems) > 5:
+    # Error checking
+    if len(problems) > 5:  # More than 5 problems error
         return 'Error: Too many problems.'
 
     problems_parts = []
     length = []
 
     for x, problem in enumerate(problems):
-        # Check input contains a + or -
-        if '+' not in problem and '-' not in problem:
+        if '+' not in problem and '-' not in problem:  # Isn't + or - sum error
             return "Error: Operator must be '+' or '-'."
-        # Split on the operand so you have problem[1] = operator and problem[0] and problem[2] being the operands
+        # problem_parts[x][1] = operator and problem[x][0], problem[x][2]= operands
         problems_parts.append(re.split(r"([+-])", problem.replace(' ', '') ))
 
         if len(problems_parts[x][0]) > len(problems_parts[x][2]):
@@ -22,35 +21,30 @@ def arithmetic_arranger(problems, calculate=False):
             length.append(len(problems_parts[x][2]))
 
         if (length[x] > 4):
-            #sys.exit('Error: Numbers cannot be more than four digits.')
             return 'Error: Numbers cannot be more than four digits.'
 
-# check longest
-    #first_longer = []
-    # # use rjust()
     i = 0
-    line1 = ''
-    line2 = ''
-    line3 = ''
-    line4 = ''
+    lines = ['']*4
     answer = 0
     for top, sign, bottom in problems_parts:
+        # Calculate sum
         if calculate is True:
             if sign == '+':
                 answer = int(top) + int(bottom)
             else:
                 answer = int(top) - int(bottom)
-            line4 += (str(answer).rjust(length[i] + 2) + '    ')
+            lines[3] += (str(answer).rjust(length[i] + 2) + '    ')
 
-        line1 += (top.rjust(length[i] + 2) + '    ')
-        line2 += (sign + bottom.rjust(length[i] + 1) + '    ')
-        line3 += ('-'*(length[i]+2) + '    ')
+        # Format output lines
+        lines[0] += (top.rjust(length[i] + 2) + '    ')
+        lines[1] += (sign + bottom.rjust(length[i] + 1) + '    ')
+        lines[2] += ('-'*(length[i]+2) + '    ')
 
         i += 1
-        # TODO make lines list, and use join to join with \n
-    if calculate is True:
-        arranged_problems = line1[:-4] + '\n' + line2[:-4] + '\n' + line3[:-4] + '\n' + line4[:-4]
-    else:
-        arranged_problems = line1[:-4] + '\n' + line2[:-4] + '\n' + line3[:-4]
 
+    # Concatenate lines into output
+    if calculate is True:
+        arranged_problems = '\n'.join(line[:-4] for line in lines)
+    else:
+        arranged_problems = '\n'.join(line[:-4] for line in lines[0:3])
     return arranged_problems
