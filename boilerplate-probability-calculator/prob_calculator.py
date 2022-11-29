2d06aa2211445662d93eebd65f148b4af3607a77
iheteroclite <118612115+iheteroclite@users.noreply.github.com>
2022-11-29 20:52:41 +0000

    First draft probability calculator
    Does not reduce the hat by the number of balls drawn, and correct probability calculation untested

--- boilerplate-probability-calculator/prob_calculator.py
+++ boilerplate-probability-calculator/prob_calculator.py
@@ -3,6 +3,35 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+	def __init__(self, **colors):
+		self.contents = [col for col in colors for x in range(colors[col])]
+
+	def draw(self, num_balls):
+	''' Will draw bals from the hat, reducing the balls in hat'''
+	#todo reduce balls in hat
+		return random.choices(self.contents, k=num_balls)
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+	counter = 0
+	# repeat for num_experiments the following steps:
+	for ex in range(num_experiments):
+		hit = False
+		# todo: can probably put it in one line?
+		# pull num_balls_drawn balls from hat
+		this_draw = hat.draw(num_balls_drawn)
+		#count each color of expected_balls in this_draw
+
+		# check whether balls are expected_balls
+		for col in expected_balls:
+			hit += this_draw.count(col) >= expected_balls[col]
+
+			#todo: try
+			# counter += 1 if hit == len(expected_balls)
+			# or
+			# counter += 1 if hit == len(expected_balls) else 0
+		if hit == len(expected_balls):
+			counter += 1
+
+
