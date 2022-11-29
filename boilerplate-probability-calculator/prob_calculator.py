2b6ed898198732d43b975efb791ae4e685173389
iheteroclite <118612115+iheteroclite@users.noreply.github.com>
2022-11-29 22:58:59 +0000

    Finished functional probability_calculator
    PASS all tests

--- boilerplate-probability-calculator/prob_calculator.py
+++ boilerplate-probability-calculator/prob_calculator.py
@@ -1,49 +1,39 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
 	def __init__(self, **colors):
 		self.contents = [col for col in colors for x in range(colors[col])]
 
 	def draw(self, num_balls):
-	#todo reduce balls in hat
-		drw = []
-
-		for b in range(num_balls):
-	#	print('lst before=', lst)
-			ball = random.choice(self.contents)
-	#	print('ball ', ball)
-			indx = self.contents.index(ball)
-	#	print('indx ', indx)
-			drw.append(self.contents.pop(indx))
-		#print ('drw ', drw)
-		#print('self.contents after=', self.contents)
+		if num_balls > len(self.contents):
+			return self.contents
+		else:
+			drw = []
+			for b in range(num_balls):
+				ball = random.choice(self.contents)
+				indx = self.contents.index(ball)
+				drw.append(self.contents.pop(indx))
 		return drw
 
-
-
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+	''' Returns the approximate decimal probability after num_experiments trys,
+		of getting expected_balls out of hat with num_balls_drawn draws'''
 	counter = 0
-	# repeat for num_experiments the following steps:
-	for ex in range(num_experiments):
-		bag = copy.deepcopy(hat)
-		hit = False
-		# todo: can probably put it in one line?
-		# pull num_balls_drawn balls from hat
-		# todo: this should draw from a copy of hat???
-		# then pop will make sense!
-		this_draw = bag.draw(num_balls_drawn)
-		#count each color of expected_balls in this_draw
 
-		# check whether balls are expected_balls
+	# Run num_experiments number of draws from the hat
+	for ex in range(num_experiments):
+		# Draw balls from a new hat
+		this_draw = copy.deepcopy(hat).draw(num_balls_drawn)
+
+		# Check whether balls are expected_balls
+		hit = False
 		for col in expected_balls:
 			hit += this_draw.count(col) >= expected_balls[col]
 
 		counter += 1 if hit == len(expected_balls) else 0
 
-	print('counter', counter)
-	print('probablility my way ', counter/num_experiments)
+	# return the probability of getting expected_balls
 	return counter/num_experiments
 
 
